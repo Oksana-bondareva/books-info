715d46b921c13ad195be71138a9b92bfa78d7194
Oksana Bondareva <oksanchik252@gmail.com>
2024-11-28 19:59:56 +0300

    feat: add likes and reviews count

--- src/components/BooksTable.tsx
+++ src/components/BooksTable.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { Container, Table, Dropdown } from "react-bootstrap";
+import { Container, Table, Dropdown, Row, Form, Col } from "react-bootstrap";
 import { Book } from "../types/types";
 import { de, fr, Faker, en, faker } from '@faker-js/faker';
 import InfiniteScroll from "react-infinite-scroll-component";
@@ -35,10 +35,11 @@ const generateReview = () => {
     };
 };
 
-const generateRandomBooks = ( page: number, language: string ): Book[] => {
+const generateRandomBooks = ( page: number, language: string, reviews: number, likes: number ): Book[] => {
     const faker = new Faker({
         locale: language === 'fr' ? [fr] : language === 'de' ? [de] : [en],
     });
+    const reviewArray = Array.from({ length: Math.ceil(reviews) }, generateReview);
 
     return Array.from({ length: 20 }, (_, index) => ({
         index: page * 20 + index + 1,
@@ -46,8 +47,8 @@ const generateRandomBooks = ( page: number, language: string ): Book[] => {
         title: capitalizeTitle(faker.word.words()),
         author: faker.person.fullName(),
         publisher: `${faker.company.name()}, ${faker.date.past().getFullYear()}`,
-        reviews: Array.from({ length: 3 }, generateReview),
-        likes: faker.number.int({ min: 0, max: 100 }),
+        reviews: reviews >= 1 ? reviewArray : (Math.random() < reviews ? [generateReview()] : []),
+        likes: Math.round((Math.random() * 2 + (likes - 1)))
     }));
 };
 
@@ -56,15 +57,17 @@ const BooksTable = () => {
     const [page, setPage] = useState(0);
     const { language, setLanguage } = useLanguage();
     const [expanded, setExpanded] = useState<{ [isbn: string]: boolean }>({});
+    const [likes, setLikes] = useState<number>(5);
+    const [reviews, setReviews] = useState<number>(5);
 
     useEffect(() => {
-        setBooks(generateRandomBooks(0, language));
-    }, [language]);
+        setBooks(generateRandomBooks(0, language, reviews, likes));
+    }, [language, reviews, likes]);
 
     const fetchMoreBooks = () => {
         setPage(prevPage => {
             const nextPage = prevPage + 1;
-            setBooks(prevBooks => [...prevBooks, ...generateRandomBooks( nextPage, language )]);
+            setBooks(prevBooks => [...prevBooks, ...generateRandomBooks(nextPage, language, reviews, likes)]);
             return nextPage;
         });
     };
@@ -77,31 +80,54 @@ const BooksTable = () => {
 
     return (
         <Container>
-           <Dropdown className="mb-3" onSelect={(e) => setLanguage(e || 'en')}>
-                <Dropdown.Toggle variant="success">
-                    Change language: {languages[language] || 'English'}
-                </Dropdown.Toggle>
-                <Dropdown.Menu>
-                    {Object.entries(languages).map(([key, value]) => (
-                        <Dropdown.Item key={key} eventKey={key}>
-                            {value}
-                        </Dropdown.Item>
-                    ))}
-                </Dropdown.Menu>
-            </Dropdown>
+            <Row className="mb-3">
+                <Col>
+                    <Form>
+                        <Row>
+                            <Col>
+                                <Dropdown onSelect={(e) => setLanguage(e || 'en')}>
+                                    <Dropdown.Toggle variant="success">
+                                        Change language: {languages[language] || 'English'}
+                                    </Dropdown.Toggle>
+                                    <Dropdown.Menu>
+                                        {Object.entries(languages).map(([key, value]) => (
+                                            <Dropdown.Item key={key} eventKey={key}>
+                                                {value}
+                                            </Dropdown.Item>
+                                        ))}
+                                    </Dropdown.Menu>
+                                </Dropdown>
+                            </Col>
+                            <Col>
+                                <Form.Group controlId="formLikes">
+                                    <Form.Label>Likes</Form.Label>
+                                    <Form.Control type="range" min="0" max="10" step="0.1" value={likes} onChange={(e) => setLikes(Number(e.target.value))} />
+                                    <Form.Control type="number" value={likes} readOnly />
+                                </Form.Group>
+                            </Col>
+                            <Col>
+                                <Form.Group controlId="formReviews">
+                                    <Form.Label>Reviews</Form.Label>
+                                    <Form.Control type="number" step="0.1" value={reviews} onChange={(e) => setReviews(Number(e.target.value))} />
+                                </Form.Group>
+                            </Col>
+                        </Row>
+                    </Form>
+                </Col>
+            </Row>
             <InfiniteScroll
                 dataLength={books.length}
                 next={fetchMoreBooks}
                 hasMore={true}
                 loader={<h4>Loading...</h4>} >
-                <Table striped bordered hover>
+                <Table striped bordered hover style={{ tableLayout: 'fixed', width: '100%' }}>
                     <thead className="table-dark">
                         <tr>
-                            <th>Index</th>
-                            <th>ISBN</th>
-                            <th>Title</th>
-                            <th>Author</th>
-                            <th>Publisher</th>
+                            <th style={{ width: '10%' }}>Index</th>
+                            <th style={{ width: '15%' }}>ISBN</th>
+                            <th style={{ width: '25%' }}>Title</th>
+                            <th style={{ width: '25%' }}>Author</th>
+                            <th style={{ width: '25%' }}>Publisher</th>
                         </tr>
                     </thead>
                     <tbody>
